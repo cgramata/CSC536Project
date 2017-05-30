6c703af2f1cb122281ab0b68b7313ea61ce92c98
cgramata <cgrama85@gmail.com>
2017-05-29 23:43:38 -0500

    Adds code to KioskActor to show its neighbors

--- src/main/scala/Application/MasterActor.scala
+++ src/main/scala/Application/MasterActor.scala
@@ -6,7 +6,7 @@ import com.typesafe.config.ConfigFactory
 import common._
 
 class MasterActor extends Actor {
-	
+
 	val numberKioskActors = ConfigFactory.load.getInt("number-kioskActors")
 	val numberClientActors = ConfigFactory.load.getInt("number-clientActors")
 	var numberTickets = ConfigFactory.load.getInt("number-TicketsA")
@@ -22,7 +22,6 @@ class MasterActor extends Actor {
 		case Neighbors(leftNeighbor, rightNeighbor) => 
 			leftNeighbor = leftNeighbor
 			rightNeighbor = rightNeighbor
-
 	}
 
 
--- src/main/scala/Common/KioskActor.scala
+++ src/main/scala/Common/KioskActor.scala
@@ -1,3 +1,20 @@
 package Common
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+import common._
+
+class KioskActor extends Actor {
+
+	var numberTicketsInKiosk = ConfigFactory.load.getInt("number-TicketsPerKiosk")
+	var leftNeighbor = self
+	var rightNeighbor = self
+
+	def receive = {
+		case Neighbors(leftNeighbor, rightNeighbor) => 
+			leftNeighbor = leftNeighbor
+			rightNeighbor = rightNeighbor
+	}
+
+}
+
