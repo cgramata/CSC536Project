e2021776c3de5005e5c47e15d11d9838916df62e
cgramata <cgrama85@gmail.com>
2017-05-29 20:48:31 -0500

    Created a folder to contain common files
    The files will be used by the MasterActor

---
+++ src/main/scala/Application/MasterActor.scala
@@ -0,0 +1,15 @@
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+import common._
+
+class MasterActor extends Actor {
+	val numberKioskActors = ConfigFactory.load.getInt("number-kioskActors")
+	val numberClientActors = ConfigFactory.load.getInt("number-clientActors")
+	val numberTickets = ConfigFactory.load.getInt("number-TicketsA")
+	var leftNeighbor = self
+	var rightNeighbor = self
+
+	def receive = {
+		case ajl;df;alsjdf
+	}
+}
