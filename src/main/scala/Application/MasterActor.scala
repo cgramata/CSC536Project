894323f768eeb61e0b7b815513f6825a13060c00
cgramata <cgrama85@gmail.com>
2017-05-29 23:59:24 -0500

    Adds code that sets neighbors up for MasterActor and KioskActors

--- src/main/scala/Application/MasterActor.scala
+++ src/main/scala/Application/MasterActor.scala
@@ -10,13 +10,28 @@ class MasterActor extends Actor {
 	val numberKioskActors = ConfigFactory.load.getInt("number-kioskActors")
 	val numberClientActors = ConfigFactory.load.getInt("number-clientActors")
 	var numberTickets = ConfigFactory.load.getInt("number-TicketsA")
-	var listOfActorRefs = new ListBuffer[ActorRef]()
+	var listOfKioskActorRefs = new ListBuffer[ActorRef]()
 	var leftNeighbor = self
 	var rightNeighbor = self
 
 	for (i <- 1 to numberKioskActors) {
-		listOfActorRefs = context.actorOf(Props[KioskActors], name = "Kiosk"+i)::listOfActorRefs
+		listOfKioskActorRefs = context.actorOf(Props[KioskActors], name = "Kiosk"+i)::listOfKioskActorRefs
 	}
+
+	for (i <- 0 to listOfKioskActorRefs.size-1) {
+		if (i == 0) {
+			listOfKioskActorRefs(i) ! Neighbors(self, listOfKioskActorRefs(i+1))
+		}
+		if (i == listOfKioskActorRefs.size-1) {
+			listOfKioskActorRefs(i) ! Neighbors(listOfKioskActorRefs(i-1), self)
+		}
+		else {
+			listOfKioskActorRefs(i) ! Neighbors(listOfKioskActorRefs(i-1), listOfKioskActorRefs(i+1))
+		}		
+	}
+
+	leftNeighbor = listOfKioskActorRefs(listOfKioskActorRefs.size-1)
+	rightNeighbor = listOfKioskActorRefs(0)
 
 	def receive = {
 		case Neighbors(leftNeighbor, rightNeighbor) => 
